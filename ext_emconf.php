31022fc297245903836e866df125c3357709c9c6
Claus Due <claus@wildside.dk>
2012-12-30 23:30:28 +0100

    [TASK] Added ext_emconf.php

---
+++ ext_emconf.php
@@ -0,0 +1,51 @@
+<?php
+
+/***************************************************************
+ * Extension Manager/Repository config file for ext "fluidcontent_bootstrap".
+ *
+ * Auto generated 16-12-2012 02:31
+ *
+ * Manual updates:
+ * Only the data in the array - everything else is removed by next
+ * writing. "version" and "dependencies" must not be touched!
+ ***************************************************************/
+
+$EM_CONF[$_EXTKEY] = array(
+	'title' => 'Fluid Content Elements: Twitter Bootstrap',
+	'description' => 'A collection of Twitter Boostrap oriented Fluid Content Elements written for EXT:fluidcontent using ViewHelpers from EXT:vhs',
+	'category' => 'misc',
+	'author' => 'Claus Due',
+	'author_email' => 'claus@wildside.dk',
+	'author_company' => 'Wildside A/S',
+	'shy' => '',
+	'dependencies' => 'cms,fluidcontent,vhs',
+	'conflicts' => '',
+	'priority' => '',
+	'module' => '',
+	'state' => 'stable',
+	'internal' => '',
+	'uploadfolder' => 0,
+	'createDirs' => '',
+	'modify_tables' => '',
+	'clearCacheOnLoad' => 1,
+	'lockType' => '',
+	'version' => '2.0.0',
+	'constraints' => array(
+		'depends' => array(
+			'typo3' => '4.5-0.0.0',
+			'cms' => '',
+			'fluidcontent' => '',
+			'vhs' => '',
+		),
+		'conflicts' => array(
+		),
+		'suggests' => array(
+			'fluidwidget' => '',
+		),
+	),
+	'suggests' => array(
+	),
+	'_md5_values_when_last_written' => '',
+);
+
+?>
