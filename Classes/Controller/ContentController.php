a123e6c7a67a733f6d927fd7ca1df6e7e750bf23
Claus Due <claus@wildside.dk>
2013-04-03 01:22:39 +0200

    [FEATURE] Implement ContentController
    …and include an extension manager update script to adjust DB records in tt_content to reflect the new logic.

---
+++ Classes/Controller/ContentController.php
@@ -0,0 +1,116 @@
+<?php
+/***************************************************************
+ *  Copyright notice
+ *
+ *  (c) 2013 Claus Due <claus@wildside.dk>, Wildside A/S
+ *
+ *  All rights reserved
+ *
+ *  This script is part of the TYPO3 project. The TYPO3 project is
+ *  free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
+ *
+ *  The GNU General Public License can be found at
+ *  http://www.gnu.org/copyleft/gpl.html.
+ *
+ *  This script is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  This copyright notice MUST APPEAR in all copies of the script!
+ ***************************************************************/
+
+/**
+ * Content Controller
+ *
+ * Controller which is used to render individual elements from
+ * the fluidcontent_bootstrap collection.
+ *
+ * @package FluidpagesBootstrap
+ * @subpackage Controller
+ * @route off
+ */
+class Tx_FluidcontentBootstrap_Controller_ContentController extends Tx_Fluidcontent_Controller_AbstractContentController {
+
+	/**
+	 * @return string
+	 */
+	public function accordionAction() {
+
+	}
+	/**
+	 * @return string
+	 */
+	public function alertAction() {
+
+	}
+	/**
+	 * @return string
+	 */
+	public function buttonGroupAction() {
+
+	}
+	/**
+	 * @return string
+	 */
+	public function buttonLinkAction() {
+
+	}
+	/**
+	 * @return string
+	 */
+	public function carouselAction() {
+
+	}
+
+	/**
+	 * @return string
+	 */
+	public function fluidRowAction() {
+
+	}
+
+	/**
+	 * @return string
+	 */
+	public function heroUnitAction() {
+
+	}
+	/**
+	 * @return string
+	 */
+	public function navigationListAction() {
+
+	}
+	/**
+	 * @return string
+	 */
+	public function pageHeaderAction() {
+
+	}
+
+	/**
+	 * @return string
+	 */
+	public function tabsAction() {
+
+	}
+
+	/**
+	 * @return string
+	 */
+	public function thumbnailsAction() {
+
+	}
+
+	/**
+	 * @return string
+	 */
+	public function wellAction() {
+
+	}
+
+}
