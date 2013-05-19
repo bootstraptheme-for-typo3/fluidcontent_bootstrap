6637f1f811bd43ca6aaf97ce591914a1e3edcf98
Claus Due <claus@wildside.dk>
2013-05-19 14:40:15 +0200

    [TER] 2.0.0

--- ext_emconf.php
+++ ext_emconf.php
@@ -3,7 +3,7 @@
 /***************************************************************
  * Extension Manager/Repository config file for ext "fluidcontent_bootstrap".
  *
- * Auto generated 22-02-2013 23:30
+ * Auto generated 19-05-2013 14:40
  *
  * Manual updates:
  * Only the data in the array - everything else is removed by next
@@ -29,7 +29,7 @@ $EM_CONF[$_EXTKEY] = array(
 	'modify_tables' => '',
 	'clearCacheOnLoad' => 1,
 	'lockType' => '',
-	'version' => '1.0.4',
+	'version' => '2.0.0',
 	'constraints' => array(
 		'depends' => array(
 			'typo3' => '4.5-0.0.0',
@@ -44,7 +44,7 @@ $EM_CONF[$_EXTKEY] = array(
 	),
 	'suggests' => array(
 	),
-	'_md5_values_when_last_written' => 'a:16:{s:12:"ext_icon.gif";s:4:"68b4";s:14:"ext_tables.php";s:4:"3498";s:9:"README.md";s:4:"b309";s:34:"Configuration/TypoScript/setup.txt";s:4:"c1ef";s:41:"Resources/Private/Elements/Accordion.html";s:4:"28e4";s:37:"Resources/Private/Elements/Alert.html";s:4:"11d9";s:43:"Resources/Private/Elements/ButtonGroup.html";s:4:"d150";s:42:"Resources/Private/Elements/ButtonLink.html";s:4:"1a3a";s:40:"Resources/Private/Elements/FluidRow.html";s:4:"c1a4";s:40:"Resources/Private/Elements/HeroUnit.html";s:4:"8e70";s:46:"Resources/Private/Elements/NavigationList.html";s:4:"b1a1";s:42:"Resources/Private/Elements/PageHeader.html";s:4:"e532";s:36:"Resources/Private/Elements/Tabs.html";s:4:"cbf4";s:42:"Resources/Private/Elements/Thumbnails.html";s:4:"c48d";s:36:"Resources/Private/Elements/Well.html";s:4:"feba";s:39:"Resources/Public/Icons/icon-twitter.png";s:4:"9450";}',
+	'_md5_values_when_last_written' => 'a:20:{s:20:"class.ext_update.php";s:4:"eed2";s:12:"ext_icon.gif";s:4:"68b4";s:14:"ext_tables.php";s:4:"a37e";s:9:"README.md";s:4:"6a33";s:40:"Classes/Controller/ContentController.php";s:4:"e80f";s:34:"Configuration/TypoScript/setup.txt";s:4:"ffa7";s:40:"Resources/Private/Language/locallang.xml";s:4:"078d";s:50:"Resources/Private/Templates/Content/Accordion.html";s:4:"b4ca";s:46:"Resources/Private/Templates/Content/Alert.html";s:4:"b126";s:52:"Resources/Private/Templates/Content/ButtonGroup.html";s:4:"f41a";s:51:"Resources/Private/Templates/Content/ButtonLink.html";s:4:"e45c";s:49:"Resources/Private/Templates/Content/Carousel.html";s:4:"5b7b";s:49:"Resources/Private/Templates/Content/FluidRow.html";s:4:"078e";s:49:"Resources/Private/Templates/Content/HeroUnit.html";s:4:"1f8e";s:55:"Resources/Private/Templates/Content/NavigationList.html";s:4:"8f0b";s:51:"Resources/Private/Templates/Content/PageHeader.html";s:4:"7dbe";s:45:"Resources/Private/Templates/Content/Tabs.html";s:4:"d699";s:51:"Resources/Private/Templates/Content/Thumbnails.html";s:4:"ceb1";s:45:"Resources/Private/Templates/Content/Well.html";s:4:"2c4a";s:39:"Resources/Public/Icons/icon-twitter.png";s:4:"9450";}',
 );
 
 ?>
