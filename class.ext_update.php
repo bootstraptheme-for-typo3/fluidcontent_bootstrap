b86f1dfb814ba36e31968771382d86eff817458a
Claus Due <claus@wildside.dk>
2013-05-26 15:22:21 +0200

    [TASK] Coding guidelines cleanup

--- class.ext_update.php
+++ class.ext_update.php
@@ -8,7 +8,7 @@ class ext_update {
 	public function main() {
 		$condition = "CType='fed_fce' AND tx_fed_fcefile LIKE 'fluidcontent_twitterbootstrap:%'";
 		$rows = $GLOBALS['TYPO3_DB']->exec_SELECTgetRows('uid,tx_fed_fcefile', 'tt_content', $condition);
-		foreach($rows as $row) {
+		foreach ($rows as $row) {
 			$newIdentity = str_replace('fluidcontent_twitterbootstrap', 'fluidcontent_bootstrap', $row['tx_fed_fcefile']);
 			$values = array(
 				'tx_fed_fcefile' => $newIdentity
@@ -16,8 +16,6 @@ class ext_update {
 			$GLOBALS['TYPO3_DB']->exec_UPDATEquery('tt_content', 'uid=' . intval($row['uid']), $values);
 		}
 
-		return count($rows) . " rows has been updated";
+		return count($rows) . ' row(s) have been updated';
 	}
 }
-
-?>
