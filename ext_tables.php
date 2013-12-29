47763a9adca48085b32424e7fa45671d3054e349
Claus Due <claus@namelesscoder.net>
2013-12-29 23:59:52 +0100

    [TASK] Switch to namespaces

--- ext_tables.php
+++ ext_tables.php
@@ -3,6 +3,6 @@ if (!defined ('TYPO3_MODE')) {
 	die ('Access denied.');
 }
 
-t3lib_extMgm::addStaticFile($_EXTKEY, 'Configuration/TypoScript', 'Fluid Content Elements: Twitter Bootstrap');
+\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addStaticFile($_EXTKEY, 'Configuration/TypoScript', 'Fluid Content Elements: Twitter Bootstrap');
 
-Tx_Flux_Core::registerProviderExtensionKey('fluidcontent_bootstrap', 'Content');
+\FluidTYPO3\Flux\Core::registerProviderExtensionKey('FluidTYPO3.FluidcontentBootstrap', 'Content');
