7f822e48edf0d528ca246fa23cf5b8c7218065d1
Claus Due <claus@namelesscoder.net>
2014-03-29 02:09:38 +0100

    [TER] 3.0.0

--- ext_emconf.php
+++ ext_emconf.php
@@ -3,7 +3,7 @@
 /***************************************************************
  * Extension Manager/Repository config file for ext "fluidcontent_bootstrap".
  *
- * Auto generated 19-05-2013 14:40
+ * Auto generated 29-03-2014 02:09
  *
  * Manual updates:
  * Only the data in the array - everything else is removed by next
@@ -14,9 +14,9 @@ $EM_CONF[$_EXTKEY] = array(
 	'title' => 'Fluid Content: Twitter Bootstrap Elements',
 	'description' => 'A collection of Twitter Bootstrap oriented Fluid Content Elements written for EXT:fluidcontent using ViewHelpers from EXT:vhs',
 	'category' => 'misc',
-	'author' => 'Claus Due',
+	'author' => 'FluidTYPO3 Team',
 	'author_email' => 'claus@namelesscoder.net',
-	'author_company' => 'Wildside A/S',
+	'author_company' => '',
 	'shy' => '',
 	'dependencies' => 'cms,fluidcontent,vhs',
 	'conflicts' => '',
@@ -29,7 +29,7 @@ $EM_CONF[$_EXTKEY] = array(
 	'modify_tables' => '',
 	'clearCacheOnLoad' => 1,
 	'lockType' => '',
-	'version' => '2.0.0',
+	'version' => '3.0.0',
 	'constraints' => array(
 		'depends' => array(
 			'typo3' => '6.1.00-6.2.99',
@@ -44,7 +44,7 @@ $EM_CONF[$_EXTKEY] = array(
 	),
 	'suggests' => array(
 	),
-	'_md5_values_when_last_written' => 'a:20:{s:20:"class.ext_update.php";s:4:"eed2";s:12:"ext_icon.gif";s:4:"68b4";s:14:"ext_tables.php";s:4:"a37e";s:9:"README.md";s:4:"6a33";s:40:"Classes/Controller/ContentController.php";s:4:"e80f";s:34:"Configuration/TypoScript/setup.txt";s:4:"ffa7";s:40:"Resources/Private/Language/locallang.xml";s:4:"078d";s:50:"Resources/Private/Templates/Content/Accordion.html";s:4:"b4ca";s:46:"Resources/Private/Templates/Content/Alert.html";s:4:"b126";s:52:"Resources/Private/Templates/Content/ButtonGroup.html";s:4:"f41a";s:51:"Resources/Private/Templates/Content/ButtonLink.html";s:4:"e45c";s:49:"Resources/Private/Templates/Content/Carousel.html";s:4:"5b7b";s:49:"Resources/Private/Templates/Content/FluidRow.html";s:4:"078e";s:49:"Resources/Private/Templates/Content/HeroUnit.html";s:4:"1f8e";s:55:"Resources/Private/Templates/Content/NavigationList.html";s:4:"8f0b";s:51:"Resources/Private/Templates/Content/PageHeader.html";s:4:"7dbe";s:45:"Resources/Private/Templates/Content/Tabs.html";s:4:"d699";s:51:"Resources/Private/Templates/Content/Thumbnails.html";s:4:"ceb1";s:45:"Resources/Private/Templates/Content/Well.html";s:4:"2c4a";s:39:"Resources/Public/Icons/icon-twitter.png";s:4:"9450";}',
+	'_md5_values_when_last_written' => 'a:34:{s:20:"class.ext_update.php";s:4:"b313";s:13:"composer.json";s:4:"1c7a";s:12:"ext_icon.gif";s:4:"68b4";s:14:"ext_tables.php";s:4:"9fcb";s:9:"README.md";s:4:"c52c";s:40:"Classes/Controller/ContentController.php";s:4:"c157";s:38:"Configuration/TypoScript/constants.txt";s:4:"5bc6";s:34:"Configuration/TypoScript/setup.txt";s:4:"8565";s:40:"Resources/Private/Language/locallang.xlf";s:4:"4ef2";s:50:"Resources/Private/Templates/Content/Accordion.html";s:4:"d806";s:46:"Resources/Private/Templates/Content/Alert.html";s:4:"f9c8";s:52:"Resources/Private/Templates/Content/ButtonGroup.html";s:4:"011c";s:51:"Resources/Private/Templates/Content/ButtonLink.html";s:4:"ecd7";s:49:"Resources/Private/Templates/Content/Carousel.html";s:4:"3f20";s:50:"Resources/Private/Templates/Content/Jumbotron.html";s:4:"0bfc";s:55:"Resources/Private/Templates/Content/NavigationList.html";s:4:"08b7";s:51:"Resources/Private/Templates/Content/PageHeader.html";s:4:"17dc";s:44:"Resources/Private/Templates/Content/Row.html";s:4:"fc28";s:45:"Resources/Private/Templates/Content/Tabs.html";s:4:"57f2";s:51:"Resources/Private/Templates/Content/Thumbnails.html";s:4:"bd24";s:45:"Resources/Private/Templates/Content/Well.html";s:4:"399d";s:41:"Resources/Public/Icons/icon-accordion.gif";s:4:"9c15";s:37:"Resources/Public/Icons/icon-alert.gif";s:4:"1a94";s:43:"Resources/Public/Icons/icon-buttongroup.gif";s:4:"76f1";s:42:"Resources/Public/Icons/icon-buttonlink.gif";s:4:"0538";s:40:"Resources/Public/Icons/icon-carousel.gif";s:4:"6a48";s:41:"Resources/Public/Icons/icon-jumbotron.gif";s:4:"72de";s:46:"Resources/Public/Icons/icon-navigationlist.gif";s:4:"acbe";s:42:"Resources/Public/Icons/icon-pageheader.gif";s:4:"b90f";s:35:"Resources/Public/Icons/icon-row.gif";s:4:"ed7e";s:36:"Resources/Public/Icons/icon-tabs.gif";s:4:"45c0";s:42:"Resources/Public/Icons/icon-thumbnails.gif";s:4:"5039";s:39:"Resources/Public/Icons/icon-twitter.png";s:4:"9450";s:36:"Resources/Public/Icons/icon-well.gif";s:4:"6ab0";}',
 );
 
 ?>
